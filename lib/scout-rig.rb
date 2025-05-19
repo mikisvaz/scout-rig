76a736d6069426e45e9d083e5bad1d421ddc1406
Miguel Vazquez <mikisvaz@gmail.com>
2025-05-19 18:58:17 +0200

    Renamed the lib path_map

--- lib/scout-rig.rb
+++ lib/scout-rig.rb
@@ -1,5 +1,5 @@
 require 'scout'
 require 'scout/path'
 require 'scout/resource'
-Path.add_path :scout_rig, File.join(Path.caller_lib_dir(__FILE__), "{TOPLEVEL}/{SUBPATH}")
+Path.add_path :scout_rig_lib, File.join(Path.caller_lib_dir(__FILE__), "{TOPLEVEL}/{SUBPATH}")
 
