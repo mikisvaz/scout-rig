56adc82cde42d1fb5ae6420c5560395bd7ba5cc4
Miguel Vazquez <mikisvaz@gmail.com>
2025-12-11 16:37:48 +0100

    Multiple value in cmd line lists

--- lib/scout/workflow/python/inputs.rb
+++ lib/scout/workflow/python/inputs.rb
@@ -15,8 +15,29 @@ module PythonWorkflow
 
       flag = "--#{name}"
       if ptype.start_with?('list[')
-        Array(val).each do |x|
-          argv << flag
+        # Accept several input formats for lists:
+        # - Ruby Array
+        # - Comma-separated string: "a,b,c"
+        # - Path to a file: "file.txt" -> read lines
+        items = []
+        if val.is_a?(String)
+          # If file exists, read lines
+          if File.exist?(val)
+            items = File.readlines(val, chomp: true)
+          elsif val.include?(',')
+            items = val.split(',').map(&:strip)
+          else
+            items = [val]
+          end
+        elsif val.respond_to?(:to_ary)
+          items = Array(val).map(&:to_s)
+        else
+          items = [val.to_s]
+        end
+
+        # pass flag once followed by all items (argparse with nargs='+' expects this)
+        argv << flag
+        items.each do |x|
           argv << x.to_s
         end
       elsif ptype == 'boolean'
@@ -26,6 +47,9 @@ module PythonWorkflow
           argv << flag if val
         end
       else
+        # For scalar inputs: if given a file path and the file exists, pass the path
+        # as-is (the Python side can decide how to handle it). Keep quoting to
+        # preserve spaces when later shell-joining.
         argv << flag
         argv << val.to_s
       end
